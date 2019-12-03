b3cbb93686a833a1c78727bee174ae2e5eb8f0b8
dongwoo <jtlas@naver.com>
2019-12-03 17:02:10 +0900

    file add

---
+++ test/AspNetCoreInDocker.Web.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+using System;
+using Xunit;
+
+namespace AspNetCoreInDocker.Web.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+			Assert.Equal(1, 1);
+
+			Assert.Equal(2, 2);
+		}
+
+		[Fact]
+        public void Test2()
+        {
+			Assert.Equal(1, 1);
+
+			Assert.Equal(2, 2);
+		}
+
+		[Fact]
+        public void Test3()
+        {
+			Assert.Equal(1, 1);
+
+			Assert.Equal(2, 2);
+		}
+    }
+}
